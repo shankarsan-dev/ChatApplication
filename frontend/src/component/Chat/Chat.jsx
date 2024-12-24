44a15a053286282467b0809d05bba961c5be91c8
noopali <k2sevak83@gmail.com>
2024-12-24 22:21:54 +0545

    scroll to bottom style changes

--- frontend/src/component/Chat/Chat.jsx
+++ frontend/src/component/Chat/Chat.jsx
@@ -1,22 +1,96 @@
 
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+// import socketIO from "socket.io-client";
+// import { user } from "../Join/Join";
+// import Message from "../Message/Message";
+// import "./Chat.css";
+// const ENDPOINT = "http://localhost:4500/";
+// let socket;
+// const Chat = () => { 
+// const [id,setId]= useState("");
+// const [messages,setMessages] = useState([1,2,3,4,3,4,52,56,65,34,243,123,54,345,345,]);
+// const send = ()=>{
+
+// const message = document.getElementById("ChatInput").value;
+// socket.emit("message",{message,id});
+// document.getElementById("ChatInput").value= null;
+
+
+
+// }
+
+//   useEffect(() => {
+//     // Initialize socket
+//     socket = socketIO(ENDPOINT, { transports: ["websocket"] });
+
+//     // Handle connection and events
+//     socket.on("connect", () => {
+//       setId(socket.id);
+//       console.log(`Connected as ${user}`);
+//       socket.emit("joined", { user });
+
+//       socket.on("welcome", (data) => {
+//         console.log(data.user, data.message);
+//       });
+
+//       socket.on("userJoined", (data) => {
+//         console.log(data.user, data.message);
+//       });
+      
+
+//       socket.on("userLeft", (data) => {
+//         console.log(data.user, data.message);
+//       });
+//     });
+
+//     // Cleanup on unmount
+//     return () => {
+//       socket.disconnect();
+//     };
+//   }, []);
+//   useEffect(()=>{
+//     socket.on("sendMessage",(data)=>{
+//       console.log(data.user,data.message,data.id);
+//     })
+//   })
+
+//   return (
+//     <div className="ChatPage">
+//       <div className="ChatContainer">
+//         <div className="Header">Header</div>
+//         <div className="ChatBox">
+//         {messages.map((item, i)=> < Message message={item}/>)}  
+//         </div>
+//         <div className="InputBox">
+//           <input type="text" className="ChatInput" id="ChatInput" placeholder="Type your message..." />
+//           <button className="SendBtn" onClick={send}>
+//             <img src="..\src\images\send.png" alt="Send" />
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Chat;
+import React, { useEffect, useRef, useState } from "react";
 import socketIO from "socket.io-client";
-import { user } from "../Join/Join";
 import Message from "../Message/Message";
 import "./Chat.css";
+
 const ENDPOINT = "http://localhost:4500/";
 let socket;
-const Chat = () => { 
-const[id,setId]= useState("");
-const send = ()=>{
 
-  const message = document.getElementById("ChatInput").value;
-socket.emit("message",{message,id});
-document.getElementById("ChatInput").value= null;
+const Chat = () => {
+  const [id, setId] = useState("");
+  const [messages, setMessages] = useState([1, 2, 3, 4, 3, 4, 52, 56, 65, 34]);
+  const chatBoxRef = useRef(null);
 
-
-
-}
+  const send = () => {
+    const message = document.getElementById("ChatInput").value;
+    socket.emit("message", { message, id });
+    document.getElementById("ChatInput").value = null;
+  };
 
   useEffect(() => {
     // Initialize socket
@@ -25,8 +99,8 @@ document.getElementById("ChatInput").value= null;
     // Handle connection and events
     socket.on("connect", () => {
       setId(socket.id);
-      console.log(`Connected as ${user}`);
-      socket.emit("joined", { user });
+      console.log(`Connected as ${id}`);
+      socket.emit("joined", { id });
 
       socket.on("welcome", (data) => {
         console.log(data.user, data.message);
@@ -35,7 +109,6 @@ document.getElementById("ChatInput").value= null;
       socket.on("userJoined", (data) => {
         console.log(data.user, data.message);
       });
-      
 
       socket.on("userLeft", (data) => {
         console.log(data.user, data.message);
@@ -47,22 +120,36 @@ document.getElementById("ChatInput").value= null;
       socket.disconnect();
     };
   }, []);
-  useEffect(()=>{
-    socket.on("sendMessage",(data)=>{
-      console.log(data.user,data.message,data.id);
-    })
-  })
+
+  useEffect(() => {
+    socket.on("sendMessage", (data) => {
+      console.log(data.user, data.message, data.id);
+    });
+  });
+
+  useEffect(() => {
+    // Scroll to the bottom when messages update
+    if (chatBoxRef.current) {
+      chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
+    }
+  }, [messages]);
 
   return (
     <div className="ChatPage">
       <div className="ChatContainer">
         <div className="Header">Header</div>
-        <div className="ChatBox">
-        <Message message = {"hey whats up"}/>
-
+        <div className="ChatBox" ref={chatBoxRef}>
+          {messages.map((item, i) => (
+            <Message key={i} message={item} />
+          ))}
         </div>
         <div className="InputBox">
-          <input type="text" className="ChatInput" id="ChatInput" placeholder="Type your message..." />
+          <input
+            type="text"
+            className="ChatInput"
+            id="ChatInput"
+            placeholder="Type your message..."
+          />
           <button className="SendBtn" onClick={send}>
             <img src="..\src\images\send.png" alt="Send" />
           </button>
